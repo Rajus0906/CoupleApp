e96cfa2a98fd45a60b406dfa549da195876a6e64
Raju sivanantham <90371622+Rajus0906@users.noreply.github.com>
2025-12-18 20:31:49 -0500

    Started App

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>I love My Wife Aadusi, this is who im making my app for</Text>
+    </View>
+  );
+}
