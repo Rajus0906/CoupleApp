22e6743faebb2bdf1a24c662b2d73b358bc50320
Raju sivanantham <rajusivanantham@gmail.com>
2026-01-07 16:01:48 -0500

    Working on the profile page and added a search section

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
 import { StyleSheet, View } from "react-native";
 
-export default function ProfileScreen() {
-    return (
+export default function SearchScreen() {
+    return(
         <View style={styles.container}>
         </View>
-    );
+    )
+
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
     container: {
         flex: 1,
         backgroundColor: '#ece0d1',
         justifyContent: "center",
         alignItems: "center",
-  },
-    text: {
-        color: '#5C4033',
-        fontSize: 20,
-  },
+    },
 
 })
