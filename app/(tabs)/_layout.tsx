f8e4f9327284fbe59beaef03c36e7f564b7fd2c7
Raju sivanantham <rajusivanantham@gmail.com>
2026-01-10 16:36:25 -0500

    added a stats section

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -27,7 +27,7 @@ export default function TabLayout () {
         <Tabs.Screen
          name="index"
          options={{
-            title: 'Home',
+            title: '',
             tabBarIcon: ({ color, focused }) => (
                 <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
             ),
@@ -47,7 +47,7 @@ export default function TabLayout () {
         <Tabs.Screen
          name="profile"
          options={{
-            title: 'Profile',
+            title: 'Raju Sivanantham',
             tabBarIcon: ({ color, focused }) => (
                 <Ionicons name={focused ? 'person-sharp' : 'person-outline'} color={color} size={24} />
             ),
